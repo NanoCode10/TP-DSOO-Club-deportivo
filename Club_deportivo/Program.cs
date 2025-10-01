75e0ba0b3dca74521a15b67b95879d6d09a31cf2
Daniela Cabrera <96794484+Dancay5071@users.noreply.github.com>
2025-10-01 12:22:04 -0300

    Correccion de errores de sintaxis

--- Club_deportivo/Program.cs
+++ Club_deportivo/Program.cs
@@ -5,10 +5,8 @@ using System.Linq;
 
 namespace Club_deportivo
 {
-    internal class Program
+    internal class Sistema
     {
-        static void Main(string[] args)
-        {
            private static List<Socio> socios = new List<Socio>();
            private static List<NoSocio> noSocios = new List<NoSocio>();
            private static List<Cuota> cuotas = new List<Cuota>();
@@ -18,33 +16,38 @@ namespace Club_deportivo
 
            public static Socio RegistrarSocio(string nombre, int dni) {
 
-                Socio nuevoSocio = new Socio(nombre, dni);
-                socios.Add(nuevoSocio)
+                int nextId = socios.Count > 0 ? socios.Max(s => s.IdSocio) + 1 : 1;
+                Socio nuevoSocio = new Socio(nombre, dni, nextId);
+                socios.Add(nuevoSocio); 
                 return nuevoSocio;
+
             }
     
-            public static NoSocio RegistrarNoSocio(string nombre, int dni) {
+          public static NoSocio RegistrarNoSocio(string nombre, int dni) {
 
-                NoSocio nuevoNoSocio = new NoSocio(nombre, dni);
-                noSocios.Add(nuevoNoSocio)
-                return nuevoNoSocio;
+                 int nextId = noSocios.Count > 0 ? noSocios.Max(n => n.IdNoSocio) + 1 : 1;
+                 NoSocio nuevoNoSocio = new NoSocio(nombre, dni, nextId);
+                 noSocios.Add(nuevoNoSocio); 
+                 return nuevoNoSocio;
+
             }
 
-            public static void CobrarCuota(int idSocio, decimal monto) {
+             public static void CobrarCuota(int idSocio, decimal monto) {
 
-                Socio socioSeleccionado = socios.FirstOrDefault(s => s.IdSocio == idSocio);
+                    Socio socioSeleccionado = socios.FirstOrDefault(s => s.IdSocio == idSocio);
 
-                DateTime fechaVencimiento = DateTime.Now.AddDays(30);
-                Cuota nuevaCuota = new Cuota(nextCuotaId++, idSocio, fechaVencimiento, monto);
+                    DateTime fechaVencimiento = DateTime.Now.AddDays(30);
+                    Cuota nuevaCuota = new Cuota(nextCuotaId++, idSocio, fechaVencimiento, monto);
 
-                cuotas.Add(nuevaCuota);
-                socioSeleccionado.Cuotas.Add(nuevaCuota);
+                    cuotas.Add(nuevaCuota);
+                    socioSeleccionado.Cuotas.Add(nuevaCuota);
 
-                socioSeleccionado.PagarCuota(nuevaCuota);
-            }
+                    socioSeleccionado.PagarCuota(nuevaCuota);
+             }
         }
     }
 
 }
+
 
 
