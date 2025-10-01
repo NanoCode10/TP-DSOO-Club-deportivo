3a980dbe8ad901bd466b60523d855d21e967b189
Daniela Cabrera <96794484+Dancay5071@users.noreply.github.com>
2025-10-01 11:51:19 -0300

    Registro de Socio y NoSocio

--- Club_deportivo/Program.cs
+++ Club_deportivo/Program.cs
@@ -9,7 +9,26 @@ namespace Club_deportivo
     {
         static void Main(string[] args)
         {
-           
+           private static List<Socio> socios = new List<Socio>();
+           private static List<NoSocio> noSocios = new List<NoSocio>();
+           private static List<Cuota> cuotas = new List<Cuota>();
+           private static List<Actividad> actividades = new List<Actividad>();
+
+
+           public static Socio RegistrarSocio(string nombre, int dni) {
+
+                Socio nuevoSocio = new Socio(nombre, dni);
+                socios.Add(nuevoSocio)
+                return nuevoSocio;
+            }
+    
+            public static NoSocio RegistrarNoSocio(string nombre, int dni) {
+
+                NoSocio nuevoNoSocio = new NoSocio(nombre, dni);
+                noSocios.Add(nuevoNoSocio)
+                return nuevoNoSocio;
+            }
         }
     }
+
 }
